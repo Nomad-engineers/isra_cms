0a12eebfd4e25ed2cd4e600b67715172f869a654
zAnz <baidosovich@gmail.com>
2026-01-13 15:01:51 +0500

    package json commands

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,9 @@
-import * as migration_20251207_163558 from './20251207_163558';
+import * as migration_20251207_163558 from './20251207_163558'
 
 export const migrations = [
   {
     up: migration_20251207_163558.up,
     down: migration_20251207_163558.down,
-    name: '20251207_163558'
+    name: '20251207_163558',
   },
-];
+]
